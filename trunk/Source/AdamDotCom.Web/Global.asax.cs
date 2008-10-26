ab77d5d95dbf3bae328dd83f92c8937b6370e57b
kahtava@gmail.com <kahtava@gmail.com@cd88c63c-9df7-11dd-b47d-f93aca0b5c28>
2008-10-26 18:28:23 +0000

    Adding baseline for IronRuby / Haml / ASP.NET MVC

---
+++ trunk/Source/AdamDotCom.Web/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using IronRubyMvc;
+using NHaml.Web.Mvc;
+
+namespace IronRubyMvcWeb
+{
+    public class GlobalApplication : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            this.InitializeIronRubyMvc();
+            ViewEngines.Engines.Add(new NHamlViewMvcEngine());
+        }
+    }
+}
