869e2cf82e72979ecb3ac4c3c386db769f0e64bc
kahtava@gmail.com <kahtava@gmail.com@cd88c63c-9df7-11dd-b47d-f93aca0b5c28>
2009-10-09 03:12:39 +0000

    New .NET MVC project.

---
+++ trunk/Source/Website/App/Models/Resume.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+using AdamDotCom_website.App.Helpers;
+
+namespace AdamDotCom_website.App.Models
+{
+    public class Resume: XDocument
+    {
+        public string ResumeFile = "Resume.xml";
+
+        public Resume()
+        {
+            if (this.IsLocal() && !this.IsStale())
+            {
+                Load(this.LocalFile());
+            }
+            else
+            {
+                XDocument.Load("http://adam.kahtava.com/services/resume/linkedin/adam-kahtava.xml");
+                Save(this.LocalFile());    
+            }
+        }
+    }
+}
