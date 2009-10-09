5fe211a8537bfc290bbef11309a13fc2922626ce
kahtava <kahtava@cd88c63c-9df7-11dd-b47d-f93aca0b5c28>
2009-10-09 22:39:36 +0000

    First pass on the resume controller

--- trunk/Source/Website/App/Controllers/ResumeController.cs
+++ trunk/Source/Website/App/Controllers/ResumeController.cs
@@ -1,15 +1,41 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Web.Mvc;
+using AdamDotCom.Resume.Service.Proxy;
+using AdamDotCom.Website.App.Helpers;
 
 namespace AdamDotCom.Website.App.Controllers
 {
+    using Resume = Resume.Service.Proxy.Resume;
+
     [HandleError]
     public class ResumeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string firstnameAndLastname)
         {
-            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+            var resume = new Resume().FromLocal();
+
+            if (resume == null)
+            {
+                resume = new Resume().FromService(firstnameAndLastname);
+            }
+            else if(resume.IsStale())
+            {
+                var callback = new AsyncCallback(AsyncServiveCall);
+            }
+
+            ViewData["Resume"] = resume;
 
             return View();
+        }
+
+        private static void AsyncServiveCall(IAsyncResult asyncResult)
+        {
+            var resume = new Resume().FromService("");
+            if (resume != null)
+            {
+                resume.SaveLocal();
+            }
         }
 
         public ActionResult About()
--- trunk/Source/Website/App/Helpers/DataExtensions.cs
+++ trunk/Source/Website/App/Helpers/DataExtensions.cs
@@ -1,11 +1,18 @@
-﻿using System.Web;
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Xml.Linq;
+using AdamDotCom.Resume.Service.Proxy;
 
 namespace AdamDotCom.Website.App.Helpers
 {
+    using Resume = Resume.Service.Proxy.Resume;
+
     public static class DataExtensions
     {
         private static string dataDirectory;
         private static int daysUntilStale;
+        private static string resumeFile = "Resume.xml";
 
         static DataExtensions()
         {
@@ -20,24 +27,36 @@ namespace AdamDotCom.Website.App.Helpers
             daysUntilStale = 2;
         }
 
-//        public static string LocalFile(this Resume resume)
-//        {
-//                return string.Format("{0}\\{1}", dataDirectory, resume.ResumeFile);
-//        }
-//
-//        public static bool IsLocal(this Resume resume)
-//        {
-//            return File.GetLastWriteTime(resume.ResumeFile) != DateTime.MinValue;
-//        }
-//
-//        public static bool IsStale(this Resume resume)
-//        {
-//            return IsStale(resume.ResumeFile);
-//        }
-//
-//        private static bool IsStale(string pathAndFile)
-//        {
-//            return File.GetLastWriteTime(pathAndFile) < DateTime.Now.AddDays(-1 * daysUntilStale);
-//        }
+        public static string LocalFile(this Resume resume)
+        {
+            return string.Format("{0}\\{1}", dataDirectory, resumeFile);
+        }
+
+        public static Resume FromService(this Resume resume, string firstnameAndLastname)
+        {
+            var service = new ResumeService();
+            return service.ResumeXml(firstnameAndLastname);
+        }
+
+        public static bool IsStale(this Resume resume)
+        {
+            return IsStale(resumeFile);
+        }
+
+        public static Resume FromLocal(this Resume resume)
+        {
+            return new Resume();
+        }
+
+        public static void SaveLocal(this Resume resume)
+        {
+            
+
+        }
+
+        private static bool IsStale(string pathAndFile)
+        {
+            return File.GetLastWriteTime(pathAndFile) < DateTime.Now.AddDays(-1 * daysUntilStale);
+        }
     }
 }
