fcf5972ce7e3e7763770f41b2a44c3a5cdea97c8
kahtava@gmail.com <kahtava@gmail.com@cd88c63c-9df7-11dd-b47d-f93aca0b5c28>
2009-10-31 17:37:30 +0000

    Wedged a default redirect to my blog http://adam.kahtava.com/journal/ from the site root.

---
+++ trunk/Source/Website/App/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace AdamDotCom.Website.App.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            HttpContext.Response.Redirect("/journal/");
+            return null;
+        }
+    }
+}
